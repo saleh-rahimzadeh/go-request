43698f3ced51d3a81971bc73974b3fd3221c26d8
Saleh Rahimzadeh <saleh-rz@outlook.com>
2025-06-29 21:57:27 +0330

    Initialization

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*┄─────────────────────────────────────────────────────────────────────────────────────────────╮
+┊   Copyright (C) 2025 Saleh Rahimzadeh                                                         │
+│   https://github.com/saleh-rahimzadeh/go-request                                              │
+│   All rights reserved.                                                                        ┊
+╰─────────────────────────────────────────────────────────────────────────────────────────────┄*/
+
+// Package request provides web request with retry.
+package request
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saleh-rahimzadeh/go-request
+
+go 1.23.0
